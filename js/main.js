96242609bd26c73ada637c0331e0d6ea52c85b07
Kirill2210 <kirill.konovalov.s3@gmail.com>
2021-08-28 21:20:56 +0500

    product and feedback

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(function(){
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerGroup: 3,
+  slidesPerView: 3,
+  autoHeight: true,
+  spaceBetween: 20,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  }
 });
